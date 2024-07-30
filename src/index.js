ebfbdfb31eca1e62b35a2e59695199a8930b9cbc
kinzun <baolei@protonmail.com>
2024-07-30 13:44:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.datanexus.top": dockerHub,
+  "quay.datanexus.top": "https://quay.io",
+  "gcr.datanexus.top": "https://gcr.io",
+  "k8s-gcr.datanexus.top": "https://k8s.gcr.io",
+  "k8s.datanexus.top": "https://registry.k8s.io",
+  "ghcr.datanexus.top": "https://ghcr.io",
+  "cloudsmith.datanexus.top": "https://docker.cloudsmith.io",
+  "ecr.datanexus.top": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
