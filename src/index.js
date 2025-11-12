ca1d3e5ff61e7ca5e5d3bdc4a3eb4c715627e61e
kinzun <baolei@protonmail.com>
2025-11-12 17:01:31 +0800

    Update routes for new CUSTOM_DOMAIN

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + "911488.xyz"]: dockerHub,
+  ["quay." + "911488.xyz"]: "https://quay.io",
+  ["gcr." + "911488.xyz"]: "https://gcr.io",
+  ["k8s-gcr." + "911488.xyz"]: "https://k8s.gcr.io",
+  ["k8s." + "911488.xyz"]: "https://registry.k8s.io",
+  ["ghcr." + "911488.xyz"]: "https://ghcr.io",
+  ["cloudsmith." + "911488.xyz"]: "https://docker.cloudsmith.io",
+  ["ecr." + "911488.xyz"]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + "911488.xyz"]: dockerHub,
 };
 
 function routeByHosts(host) {
